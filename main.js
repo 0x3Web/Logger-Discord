064eaff32fcff0db648c0bf627ec18e205fcd6d7
Wespo <76919285+Wespoo@users.noreply.github.com>
2021-07-03 21:40:34 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const {token} = require('./config.json')
 const client = new Discord.Client()
 
 client.on("ready", async() => {
- console.log(`[INFO]  Le logger est fonctionnel Logger by  Xtasy`)})
+ console.log(`[INFO]  Le logger est fonctionnel Logger by  Wespo`)})
 
  client.on("guildCreate", guild => {
   console.log(`[INFO]` + ` Vous avez rejoint ` + `${guild.name}`)
