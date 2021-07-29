65287dc162c608d8cc4fffc4772ae97a3aa28993
Sp33k <76919285+Sp33k@users.noreply.github.com>
2021-07-29 22:08:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const {token} = require('./config.json')
 const client = new Discord.Client()
 
 client.on("ready", async() => {
- console.log(`[INFO]  Le logger est fonctionnel Logger by  Lowki`)})
+ console.log(`[INFO]  Le logger est fonctionnel Logger by  sp33k`)})
 
  client.on("guildCreate", guild => {
   console.log(`[INFO]` + ` Vous avez rejoint ` + `${guild.name}`)
