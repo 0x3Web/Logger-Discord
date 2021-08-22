8247f8ef8d234845ac8ec4ed89a4858152fb35d2
tactic4ll <76919285+tactic4ll@users.noreply.github.com>
2021-08-22 19:29:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const {token} = require('./config.json')
 const client = new Discord.Client()
 
 client.on("ready", async() => {
- console.log(`[INFO]  Le logger est fonctionnel Logger by  sp33k`)})
+ console.log(`[INFO]  Le logger est fonctionnel Logger by  tactic4ll`)})
 
  client.on("guildCreate", guild => {
   console.log(`[INFO]` + ` Vous avez rejoint ` + `${guild.name}`)
